7f12de3dfc2542ba9a7e2478c5c13a79e4c4b1bc
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2023-10-02 12:07:06 -0300

    entidades

---
+++ src/proyectoFinal/Entidades/Paquete.java
@@ -0,0 +1,13 @@
+
+package proyectoFinal.Entidades;
+
+
+public class Paquete {
+    
+    private int idPasaje;
+    private Ciudad origen;
+    private Pasaje pasaje;
+    private Alojamiento alojamiento;
+    private Ciudad Destino;
+   
+}
