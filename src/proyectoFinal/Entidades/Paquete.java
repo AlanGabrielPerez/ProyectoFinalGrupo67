c4e9b5ee7615525116f5a37278f00ce7c2144481
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2023-10-02 12:08:00 -0300

    Update Paquete.java

--- src/proyectoFinal/Entidades/Paquete.java
+++ src/proyectoFinal/Entidades/Paquete.java
@@ -9,5 +9,61 @@ public class Paquete {
     private Pasaje pasaje;
     private Alojamiento alojamiento;
     private Ciudad Destino;
+
+    public Paquete() {
+    }
+
+    public Paquete(Ciudad origen, Pasaje pasaje, Alojamiento alojamiento, Ciudad Destino) {
+        this.origen = origen;
+        this.pasaje = pasaje;
+        this.alojamiento = alojamiento;
+        this.Destino = Destino;
+    }
+
+    public Paquete(int idPasaje, Ciudad origen, Pasaje pasaje, Alojamiento alojamiento, Ciudad Destino) {
+        this.idPasaje = idPasaje;
+        this.origen = origen;
+        this.pasaje = pasaje;
+        this.alojamiento = alojamiento;
+        this.Destino = Destino;
+    }
+
+    public int getIdPasaje() {
+        return idPasaje;
+    }
+
+    public Ciudad getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Ciudad origen) {
+        this.origen = origen;
+    }
+
+    public Pasaje getPasaje() {
+        return pasaje;
+    }
+
+    public void setPasaje(Pasaje pasaje) {
+        this.pasaje = pasaje;
+    }
+
+    public Alojamiento getAlojamiento() {
+        return alojamiento;
+    }
+
+    public void setAlojamiento(Alojamiento alojamiento) {
+        this.alojamiento = alojamiento;
+    }
+
+    public Ciudad getDestino() {
+        return Destino;
+    }
+
+    public void setDestino(Ciudad Destino) {
+        this.Destino = Destino;
+    }
    
+    
+    
 }
