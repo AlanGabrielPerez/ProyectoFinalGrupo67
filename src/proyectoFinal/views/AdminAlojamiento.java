b6b3db27e9e90ee3d8466c4b3fed0fd4405f3a7a
MoonshineJules <moonshineshules@gmail.com>
2023-10-09 12:24:35 -0300

    ComboBoxAlojamientoTipo

--- src/proyectoFinal/views/AdminAlojamiento.java
+++ src/proyectoFinal/views/AdminAlojamiento.java
@@ -36,11 +36,11 @@ public class AdminAlojamiento extends javax.swing.JInternalFrame {
         jLabel3 = new javax.swing.JLabel();
         jtfCiudadAlojamiento = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
         jbAlojamientoModificar = new javax.swing.JButton();
         jbAlojamientoNuevo = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
+        jcbTipoAlojamiento = new javax.swing.JComboBox<>();
 
         jLabel1.setText("Administrar Alojamiento");
 
@@ -61,8 +61,6 @@ public class AdminAlojamiento extends javax.swing.JInternalFrame {
 
         jLabel4.setText("Categoria(Tipo):");
 
-        jTextField1.setText("Hostel,Hotel,Cabaña,etc...");
-
         jbAlojamientoModificar.setText("Modificar");
 
         jbAlojamientoNuevo.setText("Nuevo");
@@ -70,6 +68,8 @@ public class AdminAlojamiento extends javax.swing.JInternalFrame {
         jLabel5.setText("Alojamiento ID:");
 
         jTextField2.setText("Ingrese ID");
+
+        jcbTipoAlojamiento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Hostel", "Hotel", "Cabaña" }));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -109,7 +109,7 @@ public class AdminAlojamiento extends javax.swing.JInternalFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel4)
                                 .addGap(18, 18, 18)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(jcbTipoAlojamiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(133, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -132,8 +132,8 @@ public class AdminAlojamiento extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                    .addComponent(jcbTipoAlojamiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbAlojamientoGuardar)
                     .addComponent(jbAlojamientoModificar)
@@ -152,13 +152,13 @@ public class AdminAlojamiento extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JButton jbAlojamientoGuardar;
     private javax.swing.JButton jbAlojamientoModificar;
     private javax.swing.JButton jbAlojamientoNuevo;
     private javax.swing.JButton jbAlojamientoSalir;
     private javax.swing.JButton jbBuscar;
+    private javax.swing.JComboBox<String> jcbTipoAlojamiento;
     private javax.swing.JTextField jtfCiudadAlojamiento;
     private javax.swing.JTextField jtfNombreAlojamiento;
     // End of variables declaration//GEN-END:variables
