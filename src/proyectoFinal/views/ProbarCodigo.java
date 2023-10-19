74d88ffa580d6554cef335e35f2b8e5f796281ea
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2023-10-18 21:29:51 -0300

    ProbarCodigo

--- src/proyectoFinal/views/ProbarCodigo.java
+++ src/proyectoFinal/views/ProbarCodigo.java
@@ -1,5 +1,6 @@
 package proyectoFinal.views;
 
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import proyectoFinal.AccessData.CiudadData;
 import proyectoFinal.AccessData.PasajeData;
@@ -39,6 +40,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         jTable = new javax.swing.JTable();
         jcbOrigen = new javax.swing.JComboBox<>();
         jtBuscar = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
 
         setClosable(true);
 
@@ -67,27 +69,39 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
             }
         });
 
+        jButton1.setText("Buscar destinos");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(92, 92, 92)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 322, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(jcbOrigen, javax.swing.GroupLayout.Alignment.LEADING, 0, 235, Short.MAX_VALUE)
-                        .addComponent(jtBuscar, javax.swing.GroupLayout.Alignment.LEADING)))
-                .addContainerGap(119, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jcbOrigen, javax.swing.GroupLayout.Alignment.LEADING, 0, 235, Short.MAX_VALUE)
+                            .addComponent(jtBuscar, javax.swing.GroupLayout.Alignment.LEADING))
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)))
+                .addContainerGap(65, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(22, Short.MAX_VALUE)
+                .addContainerGap(19, Short.MAX_VALUE)
                 .addComponent(jtBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jcbOrigen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jcbOrigen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addGap(33, 33, 33)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(109, 109, 109))
         );
@@ -113,11 +127,23 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jtBuscarKeyReleased
 
     private void jcbOrigenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcbOrigenActionPerformed
-       cargarJTable();
+       
     }//GEN-LAST:event_jcbOrigenActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        if (jcbOrigen.getSelectedIndex()<0){
+            JOptionPane.showMessageDialog(this, "Selecione una ciudad de origen.");
+        
+        }else{
+            Ciudad c = (Ciudad) jcbOrigen.getSelectedItem();
+            cargarJTable(c);
+        
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable;
     private javax.swing.JComboBox<Ciudad> jcbOrigen;
@@ -135,8 +161,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         jTable.setModel(modelo);
     }
 
-    private void cargarJTable(){
-        Ciudad origen = (Ciudad) jcbOrigen.getSelectedItem();
+    private void cargarJTable(Ciudad origen){
         modelo.setRowCount(0);
         for (Pasaje p: pd.listarPasajes()){
             if (p.isEstado() && p.getCiudadOrigen().getNombre().equals(origen.getNombre())){
