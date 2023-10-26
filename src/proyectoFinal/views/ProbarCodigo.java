b08d7c29b9f0bde20a52a6e0f5c575a23a421ff0
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2023-10-26 09:58:04 -0300

    AtrasButton

--- src/proyectoFinal/views/ProbarCodigo.java
+++ src/proyectoFinal/views/ProbarCodigo.java
@@ -1,26 +1,18 @@
 package proyectoFinal.views;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Month;
-import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumnModel;
 import proyectoFinal.AccessData.AlojamientoData;
 import proyectoFinal.AccessData.CiudadData;
 import proyectoFinal.AccessData.PasajeData;
 import proyectoFinal.Entidades.Ciudad;
 import proyectoFinal.Entidades.Paquete;
-import proyectoFinal.Entidades.Pasaje;
+import static proyectoFinal.views.Principal.Desktop;  
+import static proyectoFinal.views.Presupuesto.paquete;
 
 
 public class ProbarCodigo extends javax.swing.JInternalFrame {
@@ -28,20 +20,10 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
     CiudadData ciu = new CiudadData();
     PasajeData pd = new PasajeData();
     AlojamientoData ad = new AlojamientoData();
-    Paquete ejemplo = new Paquete();
-    
-        private static DefaultTableModel modelo = new DefaultTableModel() {
-
-        public boolean isCellEditable(int fila, int columna) {
-
-            return false;
-        }
-    };
-        
+    Paquete ejemplo = new Paquete();    
         
     public ProbarCodigo() {
         initComponents();
-
         cargarDatos(ejemplo);        
         
         jrIda.setSelected(true);
@@ -57,7 +39,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
 
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextPane = new javax.swing.JTextPane();
-        jButton1 = new javax.swing.JButton();
+        jbRegistrar = new javax.swing.JButton();
         jtEmail = new javax.swing.JTextField();
         jtPasajeros = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
@@ -67,17 +49,18 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         jlEmailValido = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jlPresupuesto = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jbAtras = new javax.swing.JButton();
+        jbConfirmar = new javax.swing.JButton();
 
         setClosable(true);
 
+        jTextPane.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
         jScrollPane2.setViewportView(jTextPane);
 
-        jButton1.setText("Confirmar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jbRegistrar.setText("Registrar");
+        jbRegistrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jbRegistrarActionPerformed(evt);
             }
         });
 
@@ -86,11 +69,6 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         jtEmail.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jtEmailMouseClicked(evt);
-            }
-        });
-        jtEmail.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jtEmailActionPerformed(evt);
             }
         });
 
@@ -124,26 +102,26 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         jlPresupuesto.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         jlPresupuesto.setText("Presupuesto");
 
-        jButton2.setText("  Atras  ");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jbAtras.setText("  Atras  ");
+        jbAtras.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jbAtrasActionPerformed(evt);
             }
         });
 
-        jButton3.setText("Comfirmar");
+        jbConfirmar.setText("Confirmar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(20, Short.MAX_VALUE)
-                .addComponent(jButton2)
+                .addContainerGap(24, Short.MAX_VALUE)
+                .addComponent(jbAtras)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton3)
+                .addComponent(jbConfirmar)
                 .addGap(14, 14, 14))
             .addGroup(layout.createSequentialGroup()
                 .addGap(37, 37, 37)
@@ -169,7 +147,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
                                         .addComponent(jlEmailValido, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(jtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                                    .addComponent(jbRegistrar, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE))))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -190,7 +168,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
                         .addComponent(jLabel1)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1)
+                    .addComponent(jbRegistrar)
                     .addComponent(jlEmailValido))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -200,8 +178,8 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jbAtras, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jbConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(23, 23, 23))
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -212,7 +190,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jbRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbRegistrarActionPerformed
         if (jtPasajeros.getText().isEmpty() || jtEmail.getText().isEmpty()) {
             JOptionPane.showMessageDialog(this, "Rellene los campos correctamente");
         } else {
@@ -226,7 +204,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
                 jlEmailValido.setVisible(true);
             }       
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jbRegistrarActionPerformed
 
     private void jrIdaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jrIdaActionPerformed
         if (jrIda.isSelected()){
@@ -252,10 +230,6 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_jtPasajerosKeyTyped
 
-    private void jtEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtEmailActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jtEmailActionPerformed
-
     private void jtEmailMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtEmailMouseClicked
         if (jtEmail.getForeground()==Color.GRAY) {       
             jtEmail.setText("");
@@ -263,20 +237,25 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_jtEmailMouseClicked
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jbAtrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbAtrasActionPerformed
+        Desktop.removeAll();
+        Desktop.repaint();
+        PresupuestoV2 ventana2= new PresupuestoV2();
+        ventana2.setVisible(true);
+        Desktop.add(ventana2);
+        Desktop.moveToFront(ventana2);
+    }//GEN-LAST:event_jbAtrasActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTextPane jTextPane;
+    private javax.swing.JButton jbAtras;
+    private javax.swing.JButton jbConfirmar;
+    private javax.swing.JButton jbRegistrar;
     private javax.swing.JLabel jlEmailValido;
     private javax.swing.JLabel jlPresupuesto;
     private javax.swing.JRadioButton jrIda;
@@ -286,7 +265,7 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
     // End of variables declaration//GEN-END:variables
 
     private void armarCabecera() {
-/*        modelo.setColumnCount(0);
+/*      modelo.setColumnCount(0);
         modelo.addColumn("id");
         modelo.addColumn("Transporte");
         modelo.addColumn("Destino");
@@ -334,38 +313,6 @@ public class ProbarCodigo extends javax.swing.JInternalFrame {
 */        
     }
     
-    public static boolean esCorreoElectronicoValido(String correo) {
-//        // Patrón para validar direcciones de correo electrónico
-//        String patron = "^[A-Za-z0-9+_.-]+@(.+)$";
-//        
-//        // Compilar el patrón
-//        Pattern pattern = Pattern.compile(patron);
-//        
-//        // Crear un objeto Matcher
-//        Matcher matcher = pattern.matcher(correo);
-//        
-//        // Verificar si el correo coincide con el patrón
-//        return matcher.matches();
-//    }
-
-//    public static void main(String[] args) {
-//        String correo1 = "usuario@example.com";
-//        String correo2 = "correoinvalido";
-//        
-//        if (esCorreoElectronicoValido(correo1)) {
-//            System.out.println(correo1 + " es un correo electrónico válido.");
-//        } else {
-//            System.out.println(correo1 + " no es un correo electrónico válido.");
-//        }
-//        
-//        if (esCorreoElectronicoValido(correo2)) {
-//            System.out.println(correo2 + " es un correo electrónico válido.");
-//        } else {
-//            System.out.println(correo2 + " no es un correo electrónico válido.");
-//        }
-   return false;
-    }
-
     private void cargarDatos(Paquete p){
         p.setAlojamiento(ad.alojamientoId(1));
         p.setOrigen(ciu.ciudadId(6));
