92dc297807f115388642af67e2dabdfba9d84ce6
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2023-10-30 11:44:43 -0300

    Update PaqueteData.java

--- src/proyectoFinal/AccessData/PaqueteData.java
+++ src/proyectoFinal/AccessData/PaqueteData.java
@@ -112,43 +112,53 @@ public class PaqueteData {
         
     }
     
-    public HashSet <Paquete> listaEmail(){
-    String sql = "SELECT  `email` FROM `paquete` WHERE `idPaquete` > 0";
-    HashSet <Paquete> hs = new HashSet<>();
+    public HashSet<String> listaEmail() {
+        String sql = "SELECT  `email` FROM `paquete` WHERE `idPaquete` > 0";
+        HashSet<String> hs = new HashSet<>();
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            
-            while (rs.next()){
-           
-            Paquete paquete = new Paquete();
-               paquete.setIdPaquete(rs.getInt("idPaquete"));
-               paquete.setOrigen(ciudad.ciudadId(rs.getInt("idOrigen")));
-               paquete.setDestino(ciudad.ciudadId(rs.getInt("idDestino")));
-               paquete.setAlojamiento(alojamiento.alojamientoId(rs.getInt("idAlojamiento")));
-               paquete.setPasaje(pasaje.pasajeId(rs.getInt("idPasaje")));
-               paquete.setFechaIn(rs.getDate("fechaIn").toLocalDate());
-               paquete.setFechaOut(rs.getDate("fechaOut").toLocalDate());
-               paquete.setEmail(rs.getString("email"));
-               paquete.setMonto(rs.getDouble("monto"));
-               paquete.setCantPasajeros(rs.getInt("cantPasajeros"));
-               hs.add(paquete);
+
+            while (rs.next()) {
+                hs.add(rs.getString("email"));
             }
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error en el sql");
         }
-     return hs;
+        return hs;
     }
     
+    
     public List <Paquete> listaPaquete(String email){
-    ArrayList<Paquete> ap = new ArrayList<>();   
-    for (Paquete p: listaEmail()){
-    if (p.getEmail().equals(email) || p.getEmail().startsWith(email)){
-    ap.add(p);
-    }
-    }    
-    return ap;
+    String sql = "SELECT * FROM `paquete` WHERE `email` = ?";
+    
+        ArrayList<Paquete> ap = new ArrayList<>();
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Paquete paquete = new Paquete();
+                paquete.setIdPaquete(rs.getInt("idPaquete"));
+                paquete.setOrigen(ciudad.ciudadId(rs.getInt("idOrigen")));
+                paquete.setDestino(ciudad.ciudadId(rs.getInt("idDestino")));
+                paquete.setAlojamiento(alojamiento.alojamientoId(rs.getInt("idAlojamiento")));
+                paquete.setPasaje(pasaje.pasajeId(rs.getInt("idPasaje")));
+                paquete.setFechaIn(rs.getDate("fechaIn").toLocalDate());
+                paquete.setFechaOut(rs.getDate("fechaOut").toLocalDate());
+                paquete.setEmail(rs.getString("email"));
+                paquete.setMonto(rs.getDouble("monto"));
+                paquete.setCantPasajeros(rs.getInt("cantPasajeros"));
+                ap.add(paquete);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error en el sql");
+        }
+
+        return ap;
     }
     
 
